aedefece33216fe42ddbea73a0a643432cb7fe71
Adwait Changan <changan.a@northeastern.edu>
2024-02-21 20:42:47 -0500

    update code

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "google_compute_network" "vpc_network" {
   for_each                = var.vpcs
   name                    = each.value.name
   auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
+  routing_mode            = var.routing_mode
   delete_default_routes_on_create = true
 }
 
@@ -35,3 +35,47 @@ resource "google_compute_route" "webapp-route" {
   dest_range       = var.webapp_route_dest_range
   next_hop_gateway = "https://www.googleapis.com/compute/v1/projects/${var.project}/global/gateways/default-internet-gateway"
 } 
+
+resource "google_compute_firewall" "webapp-firewall" {
+  name        = var.firewall_name1
+  network     = google_compute_network.vpc_network["vpc-network1"].name
+  allow {
+    protocol = "tcp"
+    ports    = [var.webapp_port1, var.webapp_port2]
+  }
+  target_tags = ["webapp"]
+  source_ranges = [var.source_ranges]
+}
+
+resource "google_compute_firewall" "webapp-firewall-deny" {
+  name        = var.firewall_name2
+  network     = google_compute_network.vpc_network["vpc-network1"].name
+  deny {
+    protocol = "tcp"
+    ports    = [22]
+  } 
+  source_tags = ["webapp"]
+  source_ranges = [var.source_ranges]
+}
+
+resource "google_compute_instance" "webapp-instance" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags = ["webapp"]
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet["subnet1"].name
+    network = google_compute_network.vpc_network["vpc-network1"].name
+    access_config {
+      network_tier = "PREMIUM"
+    }
+  }
+  boot_disk {
+    auto_delete = true
+    initialize_params {
+      size = 100
+      type = "pd-balanced"
+      image = "custom-family"
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -17,16 +17,45 @@ variable "db_ip_cidr_range" {
 variable "webapp_route_dest_range" {
 }
 
+variable "routing_mode" {
+}
+
+variable "firewall_name1" {
+  default = "webapp-firewall"
+}
+
+variable "firewall_name2" {
+  default = "webapp-firewall-deny"
+}
+
+variable "instance_name" {
+  default = "webapp-instance"
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
+
+
+variable "webapp_port1" {
+}
+
+variable "webapp_port2" {
+}
+
+variable "source_ranges" {
+}
+
 variable "vpcs" {
   default = {
     vpc-network1 = {
-      name  = "vpc-network1"
+      name = "vpc-network1"
     },
     vpc-network2 = {
-      name   = "vpc-network2"
+      name = "vpc-network2"
     },
     vpc-network3 = {
-      name  = "vpc-network3"
+      name = "vpc-network3"
     }
   }
 }
@@ -34,33 +63,33 @@ variable "vpcs" {
 variable "subnet" {
   default = {
     subnet1 = {
-      vpc = "vpc-network1"
-      name  = "webapp"
+      vpc  = "vpc-network1"
+      name = "webapp"
       cidr = "10.1.0.0/24"
     },
     subnet2 = {
-      vpc = "vpc-network1"
-      name  = "db"
+      vpc  = "vpc-network1"
+      name = "db"
       cidr = "10.2.0.0/24"
     },
     subnet3 = {
-      vpc = "vpc-network2"
-      name  = "webapp-2"
+      vpc  = "vpc-network2"
+      name = "webapp-2"
       cidr = "10.3.0.0/24"
     },
     subnet4 = {
-      vpc = "vpc-network2"
-      name  = "db-2"
+      vpc  = "vpc-network2"
+      name = "db-2"
       cidr = "10.4.0.0/24"
     },
     subnet5 = {
-      vpc = "vpc-network3"
-      name  = "webapp-3"
+      vpc  = "vpc-network3"
+      name = "webapp-3"
       cidr = "10.5.0.0/24"
     },
     subnet6 = {
-      vpc = "vpc-network3"
-      name  = "db-3"
+      vpc  = "vpc-network3"
+      name = "db-3"
       cidr = "10.6.0.0/24"
     }
   }
@@ -69,16 +98,16 @@ variable "subnet" {
 variable "webapp_route" {
   default = {
     webapp_route = {
-      name  = "webapp-route",
-      vpc = "vpc-network1"
+      name = "webapp-route",
+      vpc  = "vpc-network1"
     },
     webapp_route1 = {
-      name   = "webapp-route1",
-      vpc = "vpc-network2"
+      name = "webapp-route1",
+      vpc  = "vpc-network2"
     },
     webapp_route2 = {
-      name  = "webapp-route2",
-      vpc = "vpc-network3"
+      name = "webapp-route2",
+      vpc  = "vpc-network3"
     }
   }
 }
